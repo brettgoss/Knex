58f16fb8b9a378b6802f706572bc75609bacb560
Brett Goss <brettgoss@live.com>
2016-10-25 18:07:06 -0700

    created a script to add person to database using knex

---
+++ add-person.js
@@ -0,0 +1,27 @@
+var firstName = process.argv[2];
+var lastName  = process.argv[3];
+var birthDate = process.argv[4];
+
+const settings = require("./settings"); // settings.json
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    ssl  : true,
+    port : settings.port,
+    host : settings.hostname,
+    user : settings.user,
+    password : settings.password,
+    database : settings.database
+  }
+});
+console.log(birthDate);
+
+knex('famous_people').insert({
+  first_name: firstName,
+  last_name: lastName,
+  birthdate: birthDate
+}).asCallback();
+knex.select().from('famous_people').asCallback(function (err, result) {
+  if (err) { throw err;}
+  console.log(result);
+});
