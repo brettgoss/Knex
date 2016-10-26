1eaa4fa63d45012dc38a1de89d5f948b4b68d583
Brett Goss <brettgoss@live.com>
2016-10-25 18:06:35 -0700

    renamed files for clarity

---
+++ knex-name-finder.js
@@ -0,0 +1,28 @@
+var input = process.argv[2];
+
+const settings = require("./settings"); // settings.json
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    ssl  : true,
+    port : settings.port,
+    host : settings.hostname,
+    user : settings.user,
+    password : settings.password,
+    database : settings.database
+  }
+});
+
+knex.select().from('famous_people').where(function (){
+  this.where(
+    'first_name', 'like', input, '%'
+  ).orWhere(
+    'last_name', 'like', input, '%'
+  )
+}).timeout(1000).asCallback(function (err, result){
+  if(err){ throw err; }
+  console.log("Searching....");
+  console.log(`Found 1 person by the name '${input}':`);
+  var result = result[0];
+  console.log(`- ${result.id}: ${result.first_name} ${result.last_name}, born ${result.birthdate.toDateString()}`);
+});
